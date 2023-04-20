bb1ef17bbd16e3046a4ebe2c8c2e7b3369fb29d2
D. Neykov <metheorita@gmail.com>
2023-04-20 16:17:48 +0300

    Add JSON parsing and readable console output

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import json
+from dateutil import parser
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -10,7 +12,20 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    try:
+        parsed = json.loads(msg.payload)
+       
+        print(f'Current Temperature : {parsed["tempCurrent"]}')
+        print(f'Max Temperature : {parsed["tempMax"]}')
+        print(f'Min Temperature : {parsed["tempMin"]}')
+        #YYYY-MM-DDTHH:mm:ss.sssZ
+        print(f'Timestamp : {parser.parse(parsed["timeStamp"]).strftime("%d.%m.%Y %H:%M")}')
+        print(f'City: {parsed["city"]}\n')
+
+        #print(json.dumps(parsed, indent=3))
+    except ValueError:
+        print("Malformed Weather Data encounterd")
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
